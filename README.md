d6106061991520b19f71d809cd92b9206c2afd5f
Kagami <moe.pascal@gmail.com>
2017-06-03 23:46:01 +0800

    add usage

---
+++ README.md
@@ -0,0 +1,34 @@
+# Poi-util-transpile
+
+Util for transpiling poi plugins
+
+## compatibility
+
+This util is designed for `poi 7.6.0-beta.1` and later versions. Other versions are not guaranteed.
+
+## Howto
+
+You're expected to use `npm^5` and `node^7.6`.
+
+- install this package as dev-dependecy:
+
+```shell
+npm install --save-dev poi-util-transpile
+```
+- add prepublish and postpublish (optional) scripts in `package.json` section:
+
+```json
+    "prepublish": "poi-plugin-transpile",
+    "postpublish": "git clean -f && git checkout .",
+```
+
+## Usage
+
+```
+Usage: poi-util-transpile [source] [options]
+
+Options:
+  --sm, --source-map  save sourcemap to .js.map file                   [boolean]
+  --replace           removes .es files                                [boolean]
+  -h, --help          Show help                                        [boolean]
+```
--- index.js
+++ index.js
@@ -1,9 +1,21 @@
+#!/usr/bin/env node
 const walk = require('walk')
 const Promise = require('bluebird')
 const path = require('path')
 const fs = require('fs-extra')
+const yargs = require('yargs')
 const babel = Promise.promisifyAll(require('babel-core'))
 const { presets, plugins } = require('./babel.config')
+
+const argv = yargs.usage('Usage: $0 [source] [options]')
+  .boolean('sm')
+  .alias('sm', 'source-map')
+  .describe('sm', 'save sourcemap to .js.map file')
+  .boolean('replace')
+  .describe('replace', 'removes .es files')
+  .help('h')
+  .alias('h', 'help')
+  .argv
 
 const changeExt = (srcPath, ext) => {
   const srcDir = path.dirname(srcPath)
@@ -11,7 +23,7 @@ const changeExt = (srcPath, ext) => {
   return path.join(srcDir, srcBasename + ext)
 }
 
-const compileToJsAsync = (appDir, dontRemove) => {
+const compileToJsAsync = (appDir, replace, sm) => {
   const targetExts = ['.es']
 
   const options = {
@@ -42,8 +54,10 @@ const compileToJsAsync = (appDir, dontRemove) => {
             }
             const { code, map } = result
             await fs.writeFile(codePath, code)
-            await fs.outputJson(mapPath, map)
-            if (!dontRemove) {
+            if (sm) {
+              await fs.outputJson(mapPath, map)
+            }
+            if (replace) {
               await fs.remove(srcPath)
             }
           })
@@ -57,8 +71,15 @@ const compileToJsAsync = (appDir, dontRemove) => {
 }
 
 const main = async () => {
-  console.log(__dirname)
-  await compileToJsAsync(__dirname)
+  const source = path.resolve(process.cwd(), argv._[0] || process.cwd())
+  console.info('compiling', source)
+  if (argv.sm) {
+    console.info('Sourcemap generation enabled.')
+  }
+  if (argv.replace) {
+    console.info('Replacing .es files.')
+  }
+  await compileToJsAsync(source, argv.replace, argv.sm)
 }
 
 main()
