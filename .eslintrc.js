fbd5ee5072d2353b600c7fd042f0f3cbdfe3daa4
Kagami <moe.pascal@gmail.com>
2017-06-03 22:54:59 +0800

    ininital commit

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  'extends': 'airbnb-base',
+  'plugins': [
+    'import'
+  ],
+  'parser': 'babel-eslint',
+  'rules': {
+    'comma-dangle': ['error', 'always-multiline'],
+    'semi': ['error', 'never'],
+  }
+}
